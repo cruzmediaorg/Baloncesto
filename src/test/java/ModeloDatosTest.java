63cef045a65d5dc655c9c9a492dc1bae76bcb6a1
Luis De la Cruz <98129640+cruzmediaorg@users.noreply.github.com>
2024-01-15 11:29:53 +0100

    Sin errores

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -10,6 +10,6 @@ public class ModeloDatosTest {
  boolean expResult = false;
  boolean result = instance.existeJugador(nombre);
  assertEquals(expResult, result);
- fail("Fallo forzado.");
+//  fail("Fallo forzado.");
  }
 }
